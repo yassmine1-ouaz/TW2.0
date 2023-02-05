d98b1f51ba11dc7212524b78ae45de71337c09d5
yassmine1-ouaz <ouazyassmine@gmail.com>
2023-02-06 00:38:54 +0100

    Atelier2.2

---
+++ src/Controller/TeacherController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TeacherController extends AbstractController
+{
+    /**
+     *   @Route("/showTeacher/{name}", name ="showTeacher")
+
+     */
+    public function showTeacher($name): Response
+    {
+        return $this->render("teacher/showTeacher.html.twig");
+
+        // question 2 :dans l'url on mettre/showTeacher/yassmine : il affiche bonjour
+        return new Response("bonjour : " . $name);
+    }
+    //Question 5
+    #[Route('/goToIndex', name: 'goToStudent')]
+
+    public function goToIndex(): Response
+    {
+        return $this->redirectToRoute('app_student');
+    }
+}
