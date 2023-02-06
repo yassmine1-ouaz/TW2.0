229beec2cc45ba40a3b4dcac370c9991e8b5148e
yassmine1-ouaz <ouazyassmine@gmail.com>
2023-02-06 11:01:04 +0100

    Atelier2.2

--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -9,21 +9,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class TeacherController extends AbstractController
 {
     /**
-     *   @Route("/showTeacher/{name}", name ="showTeacher")
-
+     * @Route("/showTeacher/{name}", name ="showTeacher")
      */
     public function showTeacher($name): Response
     {
-        return $this->render("teacher/showTeacher.html.twig");
+        // nestaamlou methode hethii ou mat3 return new response
+        return $this->render("teacher/showTeacher.html.twig", ['name' => $name]);
 
         // question 2 :dans l'url on mettre/showTeacher/yassmine : il affiche bonjour
-        return new Response("bonjour : " . $name);
+        //return new Response("bonjour : " . $name);
     }
+
+
     //Question 5
-    #[Route('/goToIndex', name: 'goToStudent')]
+    #[Route("/goToIndex", name: "goToStudent")]
 
     public function goToIndex(): Response
     {
-        return $this->redirectToRoute('app_student');
+        return $this->redirectToRoute("app_student");
     }
 }
